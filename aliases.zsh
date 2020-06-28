0a35f516579ee042a7d62542b25327083ab00da8
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-06-29 00:09:23 +0200

    vim functions, aliases and prompt (still wip)

--- aliases.zsh
+++ aliases.zsh
@@ -1,16 +1,21 @@
 #vim/nvim
 alias v="nvim"
 alias svim="sudo -E nvim"
+## change directory and open files directly in vim
+alias zshrc="cd ~/.config/zsh && nvim .zshrc *.zsh"
+alias vimrc="cd ~/.vim && nvim vimrc *.vim"
+alias nburl="nvim ~/.config/newsboat/urls"
 
 #ask before removing, copying and moving
-alias rm="rm -i" 
-alias cp="cp -i" 
-alias mv="mv -i"
+alias rm="rm -Vi"
+alias cp="cp -iv"
+alias mv="mv -iv"
 
-#exa     
-alias l="exa -l"     
-alias la="exa -glah --git --icons"     
-alias ll="exa -lh --git-ignore --icons --git"  
+#exa
+#exa is a ls clone which colors the output and the permissions, can show devicons and much more
+alias l="exa -l"
+alias la="exa -glah --git --icons"
+alias ll="exa -lh --git-ignore --icons --git"
 alias lt="exa -Tlh --icons --level=3"
 
 #color aliases:
@@ -20,3 +25,6 @@ alias grep='grep --color=auto'
 
 #tmux
 alias t="tmux"
+
+#shortcuts for folders
+alias uni="cd ~/Documents/BUW && exa -l"
--- prompt.zsh
+++ prompt.zsh
@@ -1,4 +1,3 @@
-#PROMPT="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%m %{$fg[magenta]%}%~%{$fg[red]%}]%b${NEWLINE}$"
 autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
@@ -7,4 +6,4 @@ precmd_functions+=( precmd_vcs_info )
 arrow=$'\U276f'
 
 export PROMPT="%B%{$fg[blue]%}%1~%{$fg[red]%} $arrow%b"
-export RPROMPT="%~"
+export RPROMPT="%{$fg[blue]%}%~"
