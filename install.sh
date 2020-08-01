355371eb9adaec257aed59067acba0321f91ea90
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-08-02 00:00:29 +0200

    install angepasst

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#creates an .zshenv file in /home/user
 echo "creating a .zshenv in /home/\$USER/"
 echo "export ZDOTDIR=\$HOME/.config/zsh" >> ~/.zshenv
+
+#moving cloned folders to ~/.config/zsh to clean up the home directory
+location=$(pwd)
+cd ~
+mv -r . /home/\$USER/.config/zsh/
+rmdir $location
+cd /home/\$USER/.config/zsh
